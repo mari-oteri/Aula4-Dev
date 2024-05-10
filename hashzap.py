2b087a928365a526222f9938590aa32e2aa27488
mari-oteri <mariioteri1506@gmail.com>
2024-05-09 22:04:35 -0300

    First Commit - System Requirements and first imports

---
+++ hashzap.py
@@ -0,0 +1,30 @@
+# Levantar Requisitos do sistema
+    # Título do sistema: HashZap
+        # Botão : iniciar chat
+        # Pop - up ( Janela na  frente da tela)
+        # Titulo : Bem vindo ao HashZap
+        # Campo de texto : Escreva seu nome no chat - usuário se cadastra no chat
+        # Botão : Entrar no chat
+            # Sumir com o Título
+            # Fechar Pop Up
+            # Carregar o chat
+                # Mensagens que já foram envviadas
+                # Campo: Digite sua mensagem
+                # Botão: Enviar   
+
+# Ferramenta de desenvolvimento - Biblioteca : Flet (BackEnd/FrontEnd)
+# pip install flet
+
+# Passo a passo do flet 
+
+# Passo 1: Importar o flet
+import flet as ft 
+
+# Passo 2: Criar função principal (main) do seu aplicativo
+
+def main(pagina):
+    # criar todas as funcionalidades
+    
+
+# Passo 3: Rodar o app
+ft.app(main)
