c68c45590ccab2e5151de7d30d8003c0d3519501
mari-oteri <mariioteri1506@gmail.com>
2024-05-11 09:54:57 -0300

    Chat funcionando em uma página

--- hashzap.py
+++ hashzap.py
@@ -7,8 +7,9 @@
         # Botão : Entrar no chat
             # Sumir com o Título
             # Fechar Pop Up
+            # Tirar botão iniciar chat 
             # Carregar o chat
-                # Mensagens que já foram envviadas
+                # Mensagens que já foram enviadas
                 # Campo: Digite sua mensagem
                 # Botão: Enviar   
 
@@ -24,7 +25,65 @@ import flet as ft
 
 def main(pagina):
     # criar todas as funcionalidades
+    # criar um elemento
+    titulo = ft.Text("Hashzap")
+
+    # Criar janela pop up
+        # Título da janela  
+    titulo_janela = ft.Text("Bem vindo (a) ao HashZap")
+    campo_nome_usuario = ft.TextField(label="Nome de usuário")
+
+    chat = ft.Column()
+
+    def enviar_mensagem(evento):
+        texto_mensagem = campo_mensagem.value
+        nome_usuario = campo_nome_usuario.value
+        texto_chat= ft.Text(f"{nome_usuario}: {texto_mensagem}")
+        chat.controls.append(texto_chat)
+        campo_mensagem.value=""
+
+        pagina.update()
+
     
+    campo_mensagem = ft.TextField(label="Digite uma mensagem") #Adicionar on submit
+    botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+
+    linha_mensagem = ft.Row([campo_mensagem, botao_enviar_mensagem])
+    def entrar_chat(evento):
+        pagina.remove(titulo)
+        pagina.remove(botao_iniciar)
+        janela.open = False
+        pagina.add(chat)
+        pagina.add(linha_mensagem)
+        texto_entrou_chat= ft.Text(f"{campo_nome_usuario.value} entrou no chat")
+        chat.controls.append(texto_entrou_chat)
+
+        pagina.update()
+
+
+    def voltar_inicio(evento):
+        janela.open = False
+
+        pagina.update()
+
+    botao_entrar = ft.ElevatedButton("Entrar no chat", on_click=entrar_chat)
+    botao_cancelar = ft.ElevatedButton("Cancelar", on_click=voltar_inicio)
+    janela = ft.AlertDialog(title=titulo_janela,content=campo_nome_usuario, actions=[botao_entrar,botao_cancelar])
+
+    # Passo 3: Fazer função do botão
+    def iniciar_chat(evento):
+        pagina.dialog = janela
+        janela.open = True
+        pagina.update()
+
+    botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click=iniciar_chat )
+    
+    # adicionar o elemento na página
+    pagina.add(titulo)
+    pagina.add(botao_iniciar)
 
 # Passo 3: Rodar o app
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER)
+
+
+
